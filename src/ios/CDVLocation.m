c5afe4f968a031d76287b1e3beb50e8a1bc818d7
justinimel <justinimel@users.noreply.github.com>
2015-01-22 00:10:47 -0600

    Change low accuracy to stop using GPS

--- src/ios/CDVLocation.m
+++ src/ios/CDVLocation.m
@@ -154,8 +154,9 @@
     } else {
         __highAccuracyEnabled = NO;
         // TODO: Set distance filter to 10 meters? and desired accuracy to nearest ten meters? arbitrary.
-        self.locationManager.distanceFilter = 10;
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+        self.locationManager.distanceFilter = 5;
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
+        self.locationManager.pausesLocationUpdatesAutomatically = NO;
     }
 }
 
